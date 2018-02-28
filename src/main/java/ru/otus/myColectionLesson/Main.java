83beec6602c4cc63c6e9bbeb7f8ab56acae18a31
probel4uk <becclllytok@yandex.ru>
2018-02-28 19:00:06 +0300

    Initial commit

---
+++ src/main/java/ru/otus/myColectionLesson/Main.java
@@ -0,0 +1,18 @@
+package ru.otus.myColectionLesson;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Грыбочак on 23.01.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void myTest(){
+
+    }
+}
